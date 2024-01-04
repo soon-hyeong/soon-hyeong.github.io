db4813ff09326b0f78ecfc088a00faa180066b0d
Soonhyeong Joo <soonhyeongjoo@Soonhyeongs-MacBook-Pro.local>
2024-01-05 02:42:45 +0900

    add sidebar nav post number, modify sidebar nav url

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,16 @@ var store = [{
         "tags": ["test"],
         "url": "/test/test-test-test/",
         "teaser": null
+      },{
+        "title": "test2",
+        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that      Lists within lists do not break the ordered list numbering order   Your list styles go deep enough.   Ordered – Unordered – Ordered      ordered item   ordered item            unordered       unordered                    ordered item           ordered item                           ordered item   ordered item   ordered item  ","categories": ["etc"],
+        "tags": ["etc"],
+        "url": "/etc/test2/",
+        "teaser": null
+      },{
+        "title": "test2",
+        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that      Lists within lists do not break the ordered list numbering order   Your list styles go deep enough.   Ordered – Unordered – Ordered      ordered item   ordered item            unordered       unordered                    ordered item           ordered item                           ordered item   ordered item   ordered item  ","categories": ["etc"],
+        "tags": ["etc"],
+        "url": "/etc/test3/",
+        "teaser": null
       }]
