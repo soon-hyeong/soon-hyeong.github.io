a284f916530546e812400157b0ec970bf9ad50d4
Soonhyeong Joo <soonhyeongjoo@Soonhyeongs-MacBook-Pro.local>
2024-01-04 10:12:36 +0900

    add search lunr

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "test testtest",
-        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that Lists within lists do not break the ordered list numbering order Your list styles go deep enough. Ordered – Unordered – Ordered ordered item ordered item unordered unordered ordered item ordered item ordered item...","categories": ["test"],
+        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that      Lists within lists do not break the ordered list numbering order   Your list styles go deep enough.   Ordered – Unordered – Ordered      ordered item   ordered item            unordered       unordered                    ordered item           ordered item                           ordered item   ordered item  ","categories": ["test"],
         "tags": ["test"],
         "url": "/test/test-test-test/",
         "teaser": null
